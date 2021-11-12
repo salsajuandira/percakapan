a1cd1f9458949142cfc48e3ae221b09ae45233e2
salsabila juandira <92494551+salsajuandira@users.noreply.github.com>
2021-11-12 20:51:02 +0700

    Add files via upload

---
+++ 2117051012_SALSABILA JUANDIRA_POSTEST1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//salsabila juandira (2117051012)
+
+int main(){
+	
+	string variabel_nama1;
+	string variabel_nama2;
+	getline (cin,variabel_nama1);
+	getline (cin,variabel_nama2);
+	
+	cout<<endl;
+	cout<<"####Percakapan Singkat####"<<endl;
+	cout<<endl;
+	cout<<variabel_nama1<<"	: Hai salam kenal namaku \""<<variabel_nama1<<"\", nama kamu siapa?"<<endl;
+	cout<<variabel_nama2<<"	: Hai salam kenal juga namaku \""<<variabel_nama2<<"\"."<<endl;
+	return 0;
+}
+
